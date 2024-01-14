e11ffb59f8636f53a0a0d9aaf3d34811c0822796
ihsahinoglu <ihsahin63@gmail.com>
2024-01-14 03:29:09 +0300

    second

--- Find/Controllers/HomeController.cs
+++ Find/Controllers/HomeController.cs
@@ -17,12 +17,14 @@ namespace Find.Controllers
     public class HomeController : Controller
          
     {
+
         private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ApplicationDbContext context)
+        public HomeController(ILogger<HomeController> logger,ApplicationDbContext context)
         {
-            _context = context;
+			_logger = logger;
+			_context = context;
     }
 
         public IActionResult Index()
@@ -69,12 +71,13 @@ namespace Find.Controllers
         }
 
         [Route("Home/Getlist")]
-        public async Task<IActionResult> Getlist()
+        public async Task<IActionResult> Getlist(string? errorMsg)
         {
-            return View(await _context.CandidateList.ToListAsync());
+			if (errorMsg!=null) ViewData["error"] = "Hata: Deðerlerin toplamý 100 olmalýdýr.";
+			return View(await _context.CandidateList.ToListAsync());
         }
 
-        public async Task<IActionResult> Details(int? id)
+		public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
@@ -125,147 +128,128 @@ namespace Find.Controllers
 			return View(candidate);
 		}
 
+
 		[HttpPost]
-        [Route("Home/Getlist")]
-        public async Task<IActionResult> Getlist(int a, int b, int c, int d, int Age, string Gender, string MilitaryStatus, string Experience, string EducationalStatus, string Language, string Profession)
-        {
+		[Route("Home/GetFiltredList")]
+		public async Task<IActionResult> GetFiltredList(int a, int b, int c, int d, int Age, string Gender, string MilitaryStatus, string Experience, string EducationalStatus, string Language, string Profession)
+		{
 			if ((a + b + c + d) != 100 && (a + b + c + d) != 0)
 			{
-				ViewData["error"] = "Hata: Deðerlerin toplamý 100 olmalýdýr.";
-				return View(await _context.CandidateList.ToListAsync());
+				return RedirectToAction("Getlist", new { errorMsg = "Error" }); 
 			}
 
 			if (ModelState.IsValid)
 			{
 				var FiltredList = _context.CandidateList.Where(e => e.Id >= 0);
 
-					if (MilitaryStatus != "Askerlik Durumu")
-						FiltredList = FiltredList.Where(e => e.MilitaryStatus == MilitaryStatus);
+				if (MilitaryStatus != "Askerlik Durumu")
+					FiltredList = FiltredList.Where(e => e.MilitaryStatus == MilitaryStatus);
 
-					if (Experience != "Tecrübe")
-						FiltredList = FiltredList.Where(e => e.Experience == Experience);
+				if (Experience != "Tecrübe")
+					FiltredList = FiltredList.Where(e => e.Experience == Experience);
 
-					if (EducationalStatus != "Eðitim Seviyesi")
-						FiltredList = FiltredList.Where(e => e.EducationalStatus == EducationalStatus);
+				if (EducationalStatus != "Eðitim Seviyesi")
+					FiltredList = FiltredList.Where(e => e.EducationalStatus == EducationalStatus);
 
-					if (Language != "Yabancý Dil")
-						FiltredList = FiltredList.Where(e => e.Language == Language);
+				if (Language != "Yabancý Dil")
+					FiltredList = FiltredList.Where(e => e.Language == Language);
 
-					if (Gender != "Cinsiyet")
-						FiltredList = FiltredList.Where(e => e.Gender == Gender);
+				if (Gender != "Cinsiyet")
+					FiltredList = FiltredList.Where(e => e.Gender == Gender);
 
-					if (Age != 0)
-					{
+				if (Age != 0)
+				{
 
-						if (Age == 1)
-							FiltredList = FiltredList.Where(e => e.Age >= 18 && e.Age < 25);
-						if (Age == 2)
-							FiltredList = FiltredList.Where(e => e.Age >= 25 && e.Age < 35);
-						if (Age == 3)
-							FiltredList = FiltredList.Where(e => e.Age >= 35 && e.Age <= 50);
-
-					}
-
-					if (Profession != "Meslek")
-						FiltredList = FiltredList.Where(e => e.Profession == Profession);
-
-
-					foreach (var item in FiltredList)
-					{
-						int rate;
-
-						switch (item.EducationalStatus)
-						{
-							case "yüksek lisans":
-								rate = a * 5;
-								break;
-							case "lisans":
-								rate = a * 4;
-								break;
-							case "önlisans":
-								rate = a * 3;
-								break;
-							case "lise":
-								rate = a * 2;
-								break;
-							default:
-								rate = a * 1;
-								break;
-						}
-						switch (item.Experience)
-						{
-							case "10+ yýl":
-								rate += b * 5;
-								break;
-							case "5-10 yýl":
-								rate += b * 4;
-								break;
-							case "3-5 yýl":
-								rate += b * 3;
-								break;
-							case "1-3 yýl":
-								rate += b * 2;
-								break;
-							default:
-								rate += b * 1;
-								break;
-						}
-						switch (item.LanguageLevel)
-						{
-							case "B2":
-								rate += c * 5;
-								break;
-							case "B1":
-								rate += c * 4;
-								break;
-							case "A2":
-								rate += c * 3;
-								break;
-							case "A1":
-								rate += c * 2;
-								break;
-							default:
-								rate += c * 1;
-								break;
-						}
-						if (item.GraduationScore >= 3.5) rate += d * 5;
-						else if (item.GraduationScore >= 3) rate += d * 4;
-						else if (item.GraduationScore >= 2) rate += d * 3;
-						else if (item.GraduationScore >= 1) rate += d * 2;
-						else rate += d * 1;
-
-						item.Rate = rate;
-
-						Console.WriteLine(a);
-						Console.WriteLine(b);
-						Console.WriteLine(c);
-						Console.WriteLine(d);
-						Console.WriteLine(rate);
-					}
-
-					ViewData["status"] = "filtred";
-					ViewData["a"] = a;
-					ViewData["b"] = b;
-					ViewData["c"] = c;
-					ViewData["d"] = d;
-					ViewData["EducationalStatus"] = EducationalStatus;
-					ViewData["MilitaryStatus"] = MilitaryStatus;
-					ViewData["Experience"] = Experience;
-					ViewData["Age"] = Age;
-					ViewData["Gender"] = Gender;
-					ViewData["Profession"] = Profession;
-					ViewData["Language"] = Language;
-
-					FiltredList = FiltredList.OrderByDescending(e => e.Rate);
-					return View(await FiltredList.ToListAsync());
+					if (Age == 1)
+						FiltredList = FiltredList.Where(e => e.Age >= 18 && e.Age < 25);
+					if (Age == 2)
+						FiltredList = FiltredList.Where(e => e.Age >= 25 && e.Age < 35);
+					if (Age == 3)
+						FiltredList = FiltredList.Where(e => e.Age >= 35 && e.Age <= 50);
 
 				}
-			
-			return View(await _context.CandidateList.ToListAsync());
+
+				if (Profession != "Meslek")
+					FiltredList = FiltredList.Where(e => e.Profession == Profession);
+
+
+				foreach (var item in FiltredList)
+				{
+					int rate;
+
+					switch (item.EducationalStatus)
+					{
+						case "yüksek lisans":
+							rate = a * 5;
+							break;
+						case "lisans":
+							rate = a * 4;
+							break;
+						case "önlisans":
+							rate = a * 3;
+							break;
+						case "lise":
+							rate = a * 2;
+							break;
+						default:
+							rate = a * 1;
+							break;
+					}
+					switch (item.Experience)
+					{
+						case "10+ yýl":
+							rate += b * 5;
+							break;
+						case "5-10 yýl":
+							rate += b * 4;
+							break;
+						case "3-5 yýl":
+							rate += b * 3;
+							break;
+						case "1-3 yýl":
+							rate += b * 2;
+							break;
+						default:
+							rate += b * 1;
+							break;
+					}
+					switch (item.LanguageLevel)
+					{
+						case "B2":
+							rate += c * 5;
+							break;
+						case "B1":
+							rate += c * 4;
+							break;
+						case "A2":
+							rate += c * 3;
+							break;
+						case "A1":
+							rate += c * 2;
+							break;
+						default:
+							rate += c * 1;
+							break;
+					}
+					if (item.GraduationScore >= 3.5) rate += d * 5;
+					else if (item.GraduationScore >= 3) rate += d * 4;
+					else if (item.GraduationScore >= 2) rate += d * 3;
+					else if (item.GraduationScore >= 1) rate += d * 2;
+					else rate += d * 1;
+
+					item.Rate = rate;
+				}
+
+
+				var SortedandFiltredList = FiltredList.OrderByDescending(e => e.Rate);
+				return View(await SortedandFiltredList.ToListAsync());
+
+			}
+
+			return View(nameof(Getlist));
 		}
 
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
